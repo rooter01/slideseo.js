1a6521f91915c0e69f3c334623d10fa779218a08
rooter01 <rms.brandalz3@gmail.com>
2016-08-08 17:03:36 +0700

    Add files via upload

---
+++ FBPage.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+setTimeout(function(){$(".fb-likebox").each(function(){$(this).replaceWith('<iframe class="fb-likebox loader" src="'+$(this).data("src")+'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:258px;" allowTransparency="true"></iframe>')})},50);
+//]]>
