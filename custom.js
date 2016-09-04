2ef6ea9329d8aac0146f7b50d89a1a83bfab7e5d
rooter01 <rms.brandalz3@gmail.com>
2016-09-04 10:00:40 +0700

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,78 @@
+// JavaScript Document
+
+//Author Name: Saptarang
+//Author URI: http://www.saptarang.org
+//Themeforest: http://themeforest.net/user/saptarang?ref=saptarang
+//Creation Date: 18th January, 2014
+//Description: A custom script stylesheet for Missing - Responsive 404 Page Template Designed & Developed By Saptarang.
+
+
+
+/* CUSTOM PARAMETERS */
+
+/* ==================================  GLOBAL  ==================================== */
+
+			var main_color = "ffc61e";
+			
+			var page_background_color = "262626"; 
+			
+			var color2 = "373634";
+			
+			var color3 = "ffd772";
+			
+			var color4 = "a09b90";
+						
+			var Heading_Font = "Open Sans Condensed"; // thats your special Heading font
+			
+			var Site_Font = "Open Sans Condensed"; // thats your special body content font
+			
+/* ================================  SUBSCRIBE FORM  =================================== */
+			
+	/* SET SUBSCRIBE FORM SUCCESS MESSAGE */
+	
+			var sub_submitMessage = "You've subscribed successfully.";
+			
+			var sub_successParagraph = "Thank You for subscribing to our email list.";
+			
+			var sub_successBoxColor = "ffc61e"; // Background color for the success box
+			
+			var sub_successBoxBorderStyle = "solid"; // Border Style  -  Example: solid, dotted, dashed, double
+			
+			var sub_successBox_Border_Color = "000"; // Border color for success box
+			
+			var sub_textColor = "ffffff"; // text color for success box
+			
+			
+/* ================================  CONTACT FORM  =================================== */
+			
+	/* SET REGISTER FORM SUCCESS MESSAGE */
+	
+			var submitMessage = "Your form has been submitted successfully.";
+			
+			var successParagraph = "We'll respond to your message within 24 hours. Thank You!";
+			
+			var successBoxColor = "ffc61e"; // Background color for the success box
+			
+			var successBoxBorderStyle = "solid"; // Border Style  -  Example: solid, dotted, dashed, double
+			
+			var successBox_Border_Color = "777163"; // Border color for success box
+			
+			var textColor = "ffffff"; // text color for success box	
+			
+/* ================================  REPLACE GOOGLE FONT CODE HERE  =================================== */
+
+// Google Web Fonts
+
+WebFontConfig = {
+    google: { families: [ 'Open+Sans+Condensed:300,700:latin'] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+ 
